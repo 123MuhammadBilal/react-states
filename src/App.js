84705fbf7d56d81662067c3223c33b80a32cac87
Muhammad Bilal <kkmuhammadbilal2@gmail.com>
2023-05-02 15:14:43 +0500

    react states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Student from "./Students";
 
 function App() {
+  const [studentName, setStudentName] = useState('bilal');
+ function update() {
+  setStudentName('khokhar');
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Student name={studentName} />
+    <button onClick={update}>UpdateName</button>
+    </>
+   );
 }
 
 export default App;
---
+++ src/Students.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Student(props) {
+    return(
+    <h1>Student name is {props.name}</h1>
+    );
+}
+export default Student;
